e491ca1cc8668c716cbabbbcd5c057d05be3cd86
Lucas Pletsch <lucas-plets69@gmail.com>
2022-04-30 23:42:05 -0300

    Iniciando projeto

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/eminetto/curso-go/festas"
+)
 
 func main() {
-	fmt.Println("hello world")
+	casamento := festas.CalculaCasorio(festas.Parametros{
+		Adultos: 200,
+		Criancas: 30,
+		NaoAlcoolicos: 0,
+		Duracao: 8,
+		Bar: true,
+	})
+	imprimeCasorio(casamento)
+}
+
+func imprimeCasorio(c festas.Casorio){
+	un := "l"
+	fmt.Printf("Total de pessoas: %d\n", c.TPessoas)
+	fmt.Printf("Total de Agua: %d %s\n", c.TAgua, un)
+	fmt.Printf("Total de Refrigerante: %d %s\n", c.TRefri, un)
+	fmt.Printf("Total de Suco: %d %s\n", c.TSuco, un)
+	fmt.Printf("Total de Choop: %d %s\n", c.TChoop,un)
 }
